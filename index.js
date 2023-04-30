cfde16fe092ee694626f0bb71b4de8ad19b3f5d1
Aamir Hannan <aamirhannan08@gmail.com>
2023-04-30 23:28:03 +0530

    second commit

--- index.js
+++ index.js
@@ -1,48 +1,45 @@
 // ! REQUIRING MODULES
-let express = require('express'),
-	mongoose = require('mongoose'),
-	methodOverride = require('method-override'),
-	path = require('path'),
-	session = require('express-session'),
-	passport = require('passport'),
-	localStrategy = require('passport-local'),
-	moment = require('moment'),
-	flash = require('connect-flash');
+let express = require("express"),
+  mongoose = require("mongoose"),
+  methodOverride = require("method-override"),
+  path = require("path"),
+  session = require("express-session"),
+  passport = require("passport"),
+  localStrategy = require("passport-local"),
+  moment = require("moment"),
+  flash = require("connect-flash");
 let app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 // !DATABASE SETUP
-let databaseUsername = process.env.DB_USERNAME;
-let databasePassword = process.env.DB_PASS;
 mongoose
-	.connect(
-		`mongodb+srv://${databaseUsername}:${databasePassword}@hirehubdb.qt5g4th.mongodb.net/?retryWrites=true&w=majority`
-	)
-	.then(function() {
-		console.log('db working');
-	})
-	.catch(function(err) {
-		console.log(err);
-	});
+  .connect(
+    `mongodb+srv://admin:admin@cluster0.woinwo4.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(function () {
+    console.log("db working");
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
 
 // ! SESSION SETUP
-let sessionPass = process.env.SESSION_PASS;
 app.use(
-	session({
-		secret: sessionPass,
-		resave: false,
-		saveUninitialized: true,
-		cookie: {
-			httpOnly: true,
-			// secure: true,
-			expires: Date.now() + 1000 * 60 * 60 * 24,
-			maxAge: 1000 * 60 * 60 * 24
-		}
-	})
+  session({
+    secret: "SuperSecretePassword",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      // secure: true,
+      expires: Date.now() + 1000 * 60 * 60 * 24,
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
 );
 
 // ! DATABASE MODELS
-let User = require('./models/user-DB');
+let User = require("./models/user-DB");
 
 // ! PASSPORT CONFIGURATION
 app.use(passport.initialize());
@@ -52,25 +49,25 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // ! SERVER SETUP AND MIDDLEWARES
-app.use(express.static(path.join(__dirname, 'public'))); // static resources
-app.set('view engine', 'ejs'); // ejs extension
+app.use(express.static(path.join(__dirname, "public"))); // static resources
+app.set("view engine", "ejs"); // ejs extension
 app.use(express.urlencoded({ extended: true })); // body parsing
-app.use(methodOverride('_method')); // patch/delete requests
+app.use(methodOverride("_method")); // patch/delete requests
 app.use(flash()); // alerts
-app.use(function(req, res, next) {
-	res.locals.currentUser = req.user;
-	res.locals.moment = moment;
-	res.locals.error = req.flash('error');
-	res.locals.success = req.flash('success');
-	next();
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.moment = moment;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
 // ! ROUTING LOGIC
-let jobRoutes = require('./routes/jobs.js');
-let notifRoutes = require('./routes/notifications');
-let authRoutes = require('./routes/auth');
-let userRoutes = require('./routes/user');
-let questionRoutes = require('./routes/questions');
+let jobRoutes = require("./routes/jobs.js");
+let notifRoutes = require("./routes/notifications");
+let authRoutes = require("./routes/auth");
+let userRoutes = require("./routes/user");
+let questionRoutes = require("./routes/questions");
 app.use(jobRoutes);
 app.use(notifRoutes);
 app.use(authRoutes);
@@ -78,7 +75,6 @@ app.use(userRoutes);
 app.use(questionRoutes);
 
 // ! PORT SETUP
-let port = process.env.PORT;
-app.listen(port, function() {
-	console.log('server started on port 3000');
+app.listen(3000, function () {
+  console.log("server started on port 3000");
 });
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,28 +1,65 @@
-let express = require('express'),
-	jobController = require('../controllers/jobs');
+let express = require("express"),
+  jobController = require("../controllers/jobs");
 let router = express.Router();
 // model
-let Job = require('../models/job-DB.js'),
-	Notification = require('../models/notif-DB');
+let Job = require("../models/job-DB.js"),
+  Notification = require("../models/notif-DB");
 // middlewares, destructuring
-let { isLoggedIn, isAdmin } = require('../middlewares/index');
+let { isLoggedIn, isAdmin } = require("../middlewares/index");
 
-router.get('/', jobController.showLandingPage);
+router.get("/", jobController.jobIndex);
 // index
-router.get('/jobs', jobController.jobIndex);
+router.get("/jobs", jobController.jobIndex);
 // new
-router.get('/jobs/new', isLoggedIn, isAdmin, jobController.newJobForm);
+router.get("/jobs/new", isLoggedIn, isAdmin, jobController.newJobForm);
 // create
-router.post('/jobs', isLoggedIn, isAdmin, jobController.createJob);
+router.post("/jobs", isLoggedIn, isAdmin, jobController.createJob);
 // show
-router.get('/jobs/:id', jobController.showJob);
+router.get("/jobs/:id", jobController.showJob);
 // edit
-router.get('/jobs/:id/edit', isLoggedIn, isAdmin, jobController.editJobForm);
+router.get("/jobs/:id/edit", isLoggedIn, isAdmin, jobController.editJobForm);
 // update
-router.patch('/jobs/:id', isLoggedIn, isAdmin, jobController.updateJob);
+router.patch("/jobs/:id", isLoggedIn, isAdmin, jobController.updateJob);
 // delete
-router.delete('/jobs/:id', isLoggedIn, isAdmin, jobController.deleteJob);
+router.delete("/jobs/:id", isLoggedIn, isAdmin, jobController.deleteJob);
 // apply in jobs
-router.get('/jobs/:jobId/apply', isLoggedIn, jobController.applyJob);
+router.get("/jobs/:jobId/apply", isLoggedIn, jobController.applyJob);
+
+router.get("/temp", async (req, res) => {
+  for (let i = 0; i < 50; i++) {
+    try {
+      let name = "Google";
+      let address = "Delhi";
+      let image =
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/706px-Google_%22G%22_Logo.svg.png";
+      let package = 6000;
+      let cgpa = 9.1;
+      let deadline = Date.now();
+      let type = "SDE1";
+
+      // make a database object / model instance
+      let newJob = new Job({
+        name: name,
+        address: address,
+        image: image,
+        package: package,
+        cgpa: cgpa,
+        deadline: deadline,
+        type: type,
+      });
+      await newJob.save();
+      //! push a new notificatoin
+      let newNotif = new Notification({
+        body: "A new job has been posted",
+        author: newJob.name,
+      });
+      await newNotif.save();
+      req.flash("success", "job successfully posted");
+      res.redirect("/jobs");
+    } catch (error) {
+      console.log("error while adding a new job", error);
+    }
+  }
+});
 
 module.exports = router;
